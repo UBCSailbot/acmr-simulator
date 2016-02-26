8fe5c673ebfdcfb5700f8bdbda40391a673d6040
Bryan Luu <bryan.luu@uibk.ac.at>
2016-02-25 18:30:09 -0800

    Adjusted to the simulator clock to more accurately reflect the update rate of the current Ultrasonic Windsensor.

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def run():
         hardware.update()
         print "Received: Rudder Angle = " + str(gVars.bus.getData().rudder) + " ; Prop. Setting = " \
                   + str(gVars.bus.getData().sheet_percent)
-        time.sleep(0.1)
+        time.sleep(0.25)
 
 
 def getCurrentData():
